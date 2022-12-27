5075b0e045bf78b0346fdea294c9b8a76cb4c9d8
Teuton-Roy <70527737+Teuton-Roy@users.noreply.github.com>
2022-12-27 16:09:27 +0000

    DataTypes

---
+++ Besics/DataTypes.js
@@ -0,0 +1,33 @@
+//DataType in JavaScript//
+
+"use strict"; //treat all JS code as newer version of javascript//
+
+// alert(3+3)  ==>   We can't define {alert} like this in nodejs, because it's not a Browser//
+
+
+let name = "Teuton";
+let age = 22;
+let isLoggedIn = false;
+let state;
+
+
+//------------------------- Premitive DataTypes----------------------------------------------------------------//
+//number  ==>  2 to the pwer 53 (range)
+//bigInt 
+//String ==> ""
+//boolean ==> true/false
+//null ==> standalone value
+//undefined ==> variable decleared but value not decleades then it's undefined
+//symbol ==> find uniqueness
+//--------------------------------------------------------------------------------------------------------------//
+
+
+//object
+
+
+//--------------------------------------------------------------------------------------------------------------//
+console.log(typeof name);
+console.log(typeof age);
+
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined
