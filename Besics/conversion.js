ca608fc05ebeeecee4ff0ddd2205f70c35bcbb0f
Teuton-Roy <70527737+Teuton-Roy@users.noreply.github.com>
2023-01-18 13:18:59 +0000

    add some description

--- Besics/conversion.js
+++ Besics/conversion.js
@@ -8,3 +8,17 @@
 
  console.log("2" > 1);
  console.log("02" > 1);
+
+ console.log(null > 0);
+ console.log(null == 0);
+ console.log(null >= 0);
+ console.log(null <= 0); 
+ console.log(null < 0); 
+
+ /*
+    The reason is that an equality check == and comparisions > < >= <=   
+    work differently.  
+    Comparisions convert null to a number, treating it as 0.  
+    That's why (3) null >= 0 is true and (1) null > 0 is false..
+*/
+
