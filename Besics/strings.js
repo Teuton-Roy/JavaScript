59ff095c54c0fdcda5e3f3cef3324861b1e733a8
Teuton Roy <popeyehk404@gmail.com>
2023-02-25 21:31:49 +0530

    String in javascript

---
+++ Besics/strings.js
@@ -0,0 +1,16 @@
+const name = "Teuton";
+const age = 24; 
+
+//concatination [old version]
+console.log(name + age );
+
+//concatination [new version]
+// using backtick [``]
+
+console.log(`Hello my name is ${name} and my age is ${age}.`);
+
+//Another way to string declaration//
+const myName = new String("Teuton");
+
+//new keyword means, using object of javascript//
+//Behind it invocke the object//
