70dc16ef97f52b2bd5b0896db6703b980a7417af
Teuton Roy <popeyehk404@gmail.com>
2023-02-27 11:41:13 +0530

    Maths

--- Basics/num_and_math.js
+++ Basics/num_and_math.js
@@ -26,3 +26,13 @@ console.log(hundreds.toLocaleString());
 
 // ++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++//
 
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+
+console.log(Math.min(-1,-5,-6,-8));
+console.log(Math.max(8,6,8,7));
+
+
+log
