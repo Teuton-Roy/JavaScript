c10dc26593610c66d0c84a3d4357dba010fffe7e
Teuton Roy <popeyehk404@gmail.com>
2023-02-27 11:33:01 +0530

    toLocalString()

--- Basics/num_and_math.js
+++ Basics/num_and_math.js
@@ -16,3 +16,8 @@ console.log(balance.toFixed(2));
 
 const otherNumber = 123.8966
 console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
