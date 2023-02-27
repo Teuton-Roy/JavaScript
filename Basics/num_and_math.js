e1bf09bf37a90e87e8d403621e1c035f81b81ffc
Teuton Roy <popeyehk404@gmail.com>
2023-02-27 11:21:01 +0530

    toString() and toFixed() methods

--- Basics/num_and_math.js
+++ Basics/num_and_math.js
@@ -1,1 +1,14 @@
-const score = 400
+const score = 400 // js automatically detact score is Number type//
+console.log(typeof(score)) 
+
+//some time explisitly define that I want Number data-type//
+
+const balance = new Number(100)
+console.log(balance)
+
+//convert toSting//
+console.log(balance.toString());
+console.log(balance.toString().length);
+
+//set pression value//
+console.log(balance.toFixed(2));
