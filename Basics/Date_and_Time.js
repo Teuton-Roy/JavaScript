3a4a80e5a99bda3965a0ccea0fb8996b0c67d5e3
Teuton Roy <popeyehk404@gmail.com>
2023-03-01 23:31:18 +0530

    get methods

--- Basics/Date_and_Time.js
+++ Basics/Date_and_Time.js
@@ -5,7 +5,7 @@ console.log(myDate.toString()); //Wed Mar 01 2023 22:59:34 GMT+0530 (India Stand
 
 console.log(myDate.toDateString());//Wed Mar 01 2023
 console.log(myDate.toISOString()); //2023-03-01T17:31:22.942Z
-console.log(myDate.toJSON); //[Function: toJSON]
+console.log(myDate.toJSON()); //2023-03-01T17:59:47.977Z
 console.log(myDate.toLocaleString()); //3/1/2023, 10:31:22 PM
 console.log(myDate.toLocaleDateString()); //3/1/2023
 console.log(myDate.toLocaleTimeString()); //10:31:22 PM
@@ -31,4 +31,11 @@ console.log(myTimeStamp);
 console.log(myCreatedDate.getTime());
 console.log(Math.floor(Date.now() / 1000));
 
+// get
 
+let mydate = new Date();
+console.log(mydate);
+console.log(mydate.getDate());
+console.log(mydate.getMonth());
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
