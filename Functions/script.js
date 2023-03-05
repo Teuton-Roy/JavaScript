b0e62a478e5a1814fb6de4b7d460521f7c02c89f
Teuton Roy <popeyehk404@gmail.com>
2023-03-05 22:38:02 +0530

    about function()

---
+++ Functions/script.js
@@ -0,0 +1,14 @@
+console.log('hi');
+//oifngo
+//ognogno
+//ongfon
+console.log('hi')
+
+/* Two console.log() are in different places and every time I change one I need make sure  
+    I update the other one this is the perfect use-case of function().  
+
+    Because, A function() in javascript all it does take some type of logic so console.log('hi')
+    too and it just puts that inside of a particular piece of code that you can run whenever you want
+    and it always going to do the same thing every single time.
+*/
+
