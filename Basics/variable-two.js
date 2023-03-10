bb8a6f02be0e063d7f1350cb6845840c3733fb30
Teuton Roy <popeyehk404@gmail.com>
2023-03-10 19:28:04 +0530

    about promt

--- Basics/variable-two.js
+++ Basics/variable-two.js
@@ -3,5 +3,16 @@
 const uid = 'abc123'
 
 //leter on we want to change the value of uid//
-uid = 'abcd1234'
+// uid = 'abcd1234'
 //we get an error because we can't change the value of a const variable//
+
+
+let fullName = 'Teuton Roy'
+let email = 'abcd@gmail.com'
+let password = '123456'
+let confirmPassword = '123456'
+let courseCount = 0
+let isLoggedInFromGoogle = false
+
+prompt('Enter your name:')
+//promt('') is like alert('') but it takes input from the user//
