947890e3c491e83efe946a5fabf5cfe99c68c8f8
Teuton Roy <popeyehk404@gmail.com>
2023-03-10 19:23:15 +0530

    const

---
+++ Basics/variable-two.js
@@ -0,0 +1,7 @@
+//Our first user singup//
+
+const uid = 'abc123'
+
+//leter on we want to change the value of uid//
+uid = 'abcd1234'
+//we get an error because we can't change the value of a const variable//
