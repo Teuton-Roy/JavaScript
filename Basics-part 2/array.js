c26fcedb4ad4943f15910d3bc7975601a0af7d04
Teuton Roy <popeyehk404@gmail.com>
2023-03-14 21:03:12 +0530

    name changed

--- Basics-part 2/array.js
+++
@@ -1,4 +0,0 @@
-//Array//
-
-const myArr = [0, 1, 2, 3, 4, -5, 'hitesh', true]
-console.log(myArr);
---
+++ Basics-part2/array.js
@@ -0,0 +1,8 @@
+//Array//
+
+const myArr = [0, 1, 2, 3, 4, -5]
+const myHeros = ['iron-man', 'thor']
+
+const myArr2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+console.log(myArr[0]);
