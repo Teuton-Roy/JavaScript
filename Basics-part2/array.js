655c91ac64a557e0ceccf993cf39a5ce799925b8
Teuton Roy <popeyehk404@gmail.com>
2023-03-14 21:16:11 +0530

    array.unshift

--- Basics-part2/array.js
+++ Basics-part2/array.js
@@ -13,11 +13,16 @@ const myArr2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 //Array Methods//
 // 1. myArr.push() : add new elements add to the array//
 
-myArr.push(6)
-myArr.push(7)
-console.log(myArr);
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr)
 
 //2. myArr.pop() :  no argument need, remove last value from the array.
 
-myArr.pop()
-console.log(myArr);
+// myArr.pop()
+// console.log(myArr)
+
+//3. myArr.unshife() : add new elements at the begining of the array.
+
+myArr.unshift(-1)
+console.log(myArr)
