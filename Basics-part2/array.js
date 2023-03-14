602b06842361199791510279eb605925a26be960
Teuton Roy <popeyehk404@gmail.com>
2023-03-14 21:35:20 +0530

    ..

--- Basics-part2/array.js
+++ Basics-part2/array.js
@@ -5,56 +5,56 @@ const myHeros = ['iron-man', 'thor']
 
 const myArr2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-// console.log(myArr[0]);
-// console.log(myHeros[1]);
+console.log(myArr[0]);
+console.log(myHeros[1]);
 
-// console.log(myArr2[5]);
+console.log(myArr2[5]);
 
 //Array Methods//
 // 1. myArr.push() : add new elements add to the array//
 
-// myArr.push(6)
-// myArr.push(7)
-// console.log(myArr)
+myArr.push(6)
+myArr.push(7)
+console.log(myArr)
 
 //2. myArr.pop() :  no argument need, remove last value from the array.
 
-// myArr.pop()
-// console.log(myArr)
+myArr.pop()
+console.log(myArr)
 
 //3. myArr.unshife() : add new elements at the begining of the array.
 
-// myArr.unshift(-1)
-// console.log(myArr)  
+myArr.unshift(-1)
+console.log(myArr)  
 
 //4. myArr.shift() : remove first element from the array.
 
-// myArr.shift()
-// console.log(myArr)
+myArr.shift()
+console.log(myArr)
 
 //5. myArr.includes() : check if the value is present in the array or not.
 
-// console.log(myArr.includes(4))
+console.log(myArr.includes(4))
 
 //6. myArr.indexOf() : check the index of the value in the array.
 
-// console.log(myArr.indexOf(9))
+console.log(myArr.indexOf(9))
 
 //7. myArr.join() : convert array to string.
 
-// const newArr = myArr.join()
-// console.log(newArr)
-// console.log(typeof newArr);
+const newArr1 = myArr.join()
+console.log(newArr1)
+console.log(typeof newArr1);
 
 //8. myArr.slice() : copy the array.
 
-// const newArr = myArr.slice(0, 3)
-// console.log(newArr);
-// console.log('B', myArr);
+const newArr2 = myArr.slice(0, 3)
+console.log(newArr2);
+console.log('B', myArr2);
 
 //9. myArr.splice() : remove the element from the array.
 
 console.log(myArr);
-const newArr = myArr.splice(0, 3)
+const newArr3 = myArr.splice(0, 3)
 console.log('C', myArr);
-console.log(newArr);
+console.log(newArr3);
