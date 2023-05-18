034af846c13fc3d15b268ba7a07cd79bf648ae22
Teuton Roy <popeyehk404@gmail.com>
2023-05-18 20:21:10 +0530

    load_script_using_promise

---
+++ Promise/Load_Script_Using_Promise/script.js
@@ -0,0 +1,20 @@
+const loadScript = (src) => {
+    return new Promise((resolve, reject) => {
+        let script = document.createElement('script');
+        script.src = src;
+        document.body.appendChild(script)
+        script.onload = () => {
+            resolve(1)
+        }
+        script.onerror = () => {
+            reject(0)
+        }
+    })
+}
+
+let promise1 = loadScript('http://localhost:5500/index.js')
+promise1.then((value) => {
+    console.log(value)
+}).catch((error) => {
+    console.log('we are sorry butwe are having problems with the loading of the script')
+})
