8f88bb00f7d3772d4d40c4fd42cb8816843b7fd5
Teuton Roy <popeyehk404@gmail.com>
2023-05-18 19:46:27 +0530

    .

---
+++ Promise/Promise/script.js
@@ -0,0 +1,38 @@
+//resolve
+let promise = new Promise(function(resolve, reject){
+    
+    console.log("Promise is in pending state")
+    setTimeout(()=>{
+        // console.log('I am in Promise and I am resolve')
+        resolve(true)
+    },5000)
+})
+// console.log(promise)
+
+//Reject
+let promise1 = new Promise(function(resolve, reject){
+    
+    console.log("Promise is in pending state")
+    setTimeout(()=>{
+        // console.log('I am in Promise and I am rejected')
+        reject(new Error('I am rejected'))
+  },5000)
+})
+// console.log(promise, promise1)
+
+//.then()
+promise.then((value) => {
+    console.log(value)
+})
+
+//.catch()
+// promise1.catch((error) => {
+//     console.log('somthing went wrong in Promise1');
+// })
+
+//.then() and .catch() together
+promise1.then((value) => {
+    console.log(value)
+}, (error) => {
+    console.log('somthing went wrong in Promise1');
+})
---
+++ Promise/promises_in_js.js
@@ -0,0 +1,43 @@
+/* 
+    What is a promise?
+    
+    In simple example,
+    with our daily-life, that, we orderd food from Zomato and Swiggy, and they promised us to deliver the food within few minutes.
+    Then we do our task.
+
+    So, in JavaScript, 
+    Promise is a promise of code excution, that, it will be executed in future.
+    The code either executed successfully or failed. 
+    In both the cases, the user will be notified.
+
+    Syntax:
+    let Promise = new Promise(function(resolve, reject){
+        // executor (the producing code)
+    })
+
+    Properties:
+    In promise object, there are 2 properties:
+
+    1. State: Initially panding, then changes to either "fulfiled" when resolve is called or "rejected" when reject is called.
+
+    2. Result: Initially undefined, then changes to either "value" when resolve(value) called or "error" when reject(error) called.
+
+
+    Promise mainly used for Parallel execution.
+
+*/
+
+
+let promise = new Promise(function(resolve, reject){
+    resolve(56)
+})
+
+
+console.log('Hello, Teuton 1st')
+
+setTimeout(function(){
+    console.log("Hola, Teuton after 2 seconds 2nd")
+},2000)
+
+console.log("My name is Teuton Roy, and I am a Web Developer 3rd")
+console.log(promise)
