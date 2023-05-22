976d42b9c6ff5ec5b3b43a4075be1abb920ab32a
Teuton Roy <popeyehk404@gmail.com>
2023-05-22 22:23:35 +0530

    objects

---
+++ Basics-part2/objects.js
@@ -0,0 +1,32 @@
+// Singleton: only one instance of an object is allowed.
+
+// Object literals 
+
+
+const myUser = {
+    name: "Teuton Roy",
+    DOB: "18-03-2000",
+    email:"2021pgcaca070@nitjsr.ac.in",
+    phone: 8972028100,
+    isLoggedIn: false,
+    courses: ["DSA", "Web Dev", "React"],
+    login: function(){
+        this.isLoggedIn = true;
+    },
+    logout: function(){
+        this.isLoggedIn = false;
+    },
+
+    lastLogin: [
+        [
+            "18-03-2021",
+            "21:40:58"
+        ],
+        [
+            "19-03-2021",
+            "8:25:10"
+        ]
+    ]
+}
+
+console.log(myUser);
