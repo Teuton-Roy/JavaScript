2a2d9862d18b72fabbc01647dd084dbe772509a7
Teuton Roy <popeyehk404@gmail.com>
2023-05-24 18:59:51 +0530

    Singleton & non-singleton pattern

--- Basics-part2/object2.js
+++ Basics-part2/object2.js
@@ -1,2 +1,7 @@
 //About Singleton Pattern..
 //Declare object using Constructor..
+
+const myappUser = new Object() //Singleton Pattern..
+const myapp = {} // non-singleton pattern..
+
+console.log(myappUser)
