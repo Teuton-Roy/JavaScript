b8d850f093ec66c71358ceccd5d5f1417176fd30
Teuton Roy <popeyehk404@gmail.com>
2023-05-24 19:02:31 +0530

    ..

--- Basics-part2/object2.js
+++ Basics-part2/object2.js
@@ -1,7 +1,13 @@
 //About Singleton Pattern..
 //Declare object using Constructor..
 
+
+// const myapp = {} // non-singleton pattern..
+
+
 const myappUser = new Object() //Singleton Pattern..
-const myapp = {} // non-singleton pattern..
+myappUser.id = "@xyz1"
+myappUser.name = "Teuton Roy"
+myappUser.isLoggedIn = false
 
 console.log(myappUser)
