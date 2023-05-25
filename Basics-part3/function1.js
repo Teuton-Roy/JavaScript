b30b80a524c86130ee13df7f18739b0ce6886923
Teuton Roy <popeyehk404@gmail.com>
2023-05-25 19:23:56 +0530

    ..

---
+++ Basics-part3/function1.js
@@ -0,0 +1,12 @@
+//Functions
+
+function sayMyName(){
+    console.log("T");
+    console.log("E");
+    console.log("u");
+    console.log("T");
+    console.log("O");
+    console.log("N");
+}
+
+sayMyName()
