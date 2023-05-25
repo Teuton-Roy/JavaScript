7880527519b2528e4abc0edbf835e80c33e148ea
Teuton Roy <popeyehk404@gmail.com>
2023-05-25 19:33:10 +0530

    ..

--- Basics-part3/function1.js
+++ Basics-part3/function1.js
@@ -9,4 +9,27 @@ function sayMyName(){
     console.log("N");
 }
 
-sayMyName()
+// sayMyName()
+
+function addTwoNumbers(number1, number2){
+    /*
+        when we create function defination like:
+        function abc(){
+
+        }
+
+        If here in between () we pass anything it called Parameter, like:
+        function abc(parameter, parameter){
+
+        }
+    */ 
+
+    console.log(number1 + number2)
+}
+
+// addTwoNumbers() return NaN Cause, noargument passing..
+addTwoNumbers(3, 7)
+/*
+    when we call the function then it's called Argument..
+    addTwoNumbers(argument, argument)
+*/
