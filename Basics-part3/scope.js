daaecf5142a38b72df6004d6ca986a6c3a7d330b
Teuton Roy <popeyehk404@gmail.com>
2023-05-26 22:56:23 +0530

    ..

--- Basics-part3/scope.js
+++ Basics-part3/scope.js
@@ -1,14 +1,19 @@
 //Global Scope
 var c = 300
 
+let a = 200
+
+
 // Block Scope
 if (true) {
     let a = 10;
     const b = 20;
-    var c = 30; 
+    // var c = 30; 
+
+    console.log("Inner value: "+a);
 }
 
 
 // console.log(a);
 // console.log(b);
-console.log(c); //this the problem of the var 
+console.log(a); //this the problem of the var 
