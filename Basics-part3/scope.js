f4f422cd4184b2327c3403cad02bb49d67697b03
Teuton Roy <popeyehk404@gmail.com>
2023-05-26 22:54:29 +0530

    ..

--- Basics-part3/scope.js
+++ Basics-part3/scope.js
@@ -1,9 +1,14 @@
-// Scope
+//Global Scope
+var c = 300
 
-let a = 10;
-const b = 20;
-var c = 30;
+// Block Scope
+if (true) {
+    let a = 10;
+    const b = 20;
+    var c = 30; 
+}
 
-console.log(a);
-console.log(b);
-console.log(c);
+
+// console.log(a);
+// console.log(b);
+console.log(c); //this the problem of the var 
