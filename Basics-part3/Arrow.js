a1cfececcb0db0940e7689eb5e2a50d035f26d0e
Teuton Roy <popeyehk404@gmail.com>
2023-05-28 19:14:13 +0530

    ...

--- Basics-part3/Arrow.js
+++ Basics-part3/Arrow.js
@@ -2,10 +2,14 @@ const User = {
     username : "Teuton Roy",
     price: '299',
     welcomeMessage: function(){
-        console.log(`${this.username}, welcome to website`);
+        console.log(`${this.username}, welcome to website.`);
     }
     /*
         Within this scope, for access every veriable we use [this.] keyword  
-        [this.] keyword use for current context
+        [this.] keyword use for current values
     */
 }
+User.welcomeMessage() //this welcome message for Teuton
+
+User.username = 'laura' //here, I change the username context/value
+User.welcomeMessage() //now,this welcome message for laura
