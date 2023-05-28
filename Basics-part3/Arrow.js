b170c42e5508dec97712d506894e82d3c1bdf7fc
Teuton Roy <popeyehk404@gmail.com>
2023-05-28 19:23:59 +0530

    ..

--- Basics-part3/Arrow.js
+++ Basics-part3/Arrow.js
@@ -3,6 +3,7 @@ const User = {
     price: '299',
     welcomeMessage: function(){
         console.log(`${this.username}, welcome to website.`);
+        console.log(this) //show current context..
     }
     /*
         Within this scope, for access every veriable we use [this.] keyword  
@@ -13,3 +14,11 @@ User.welcomeMessage() //this welcome message for Teuton
 
 User.username = 'laura' //here, I change the username context/value
 User.welcomeMessage() //now,this welcome message for laura
+
+console.log(this); 
+/*
+    here the out is empty object cause, in global there is no object..
+    
+    it return the empty object cause it execute in node platform,
+    if we execute in our browser console then it gives some output...
+*/
