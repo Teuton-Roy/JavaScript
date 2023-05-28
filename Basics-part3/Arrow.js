e3c5d401ccfcb68f0ee4264b0097e7015ff283c6
Teuton Roy <popeyehk404@gmail.com>
2023-05-28 19:30:18 +0530

    ..

--- Basics-part3/Arrow.js
+++ Basics-part3/Arrow.js
@@ -3,22 +3,30 @@ const User = {
     price: '299',
     welcomeMessage: function(){
         console.log(`${this.username}, welcome to website.`);
-        console.log(this) //show current context..
+        // console.log(this) //show current context..
     }
     /*
         Within this scope, for access every veriable we use [this.] keyword  
         [this.] keyword use for current values
     */
 }
-User.welcomeMessage() //this welcome message for Teuton
+// User.welcomeMessage() //this welcome message for Teuton
 
-User.username = 'laura' //here, I change the username context/value
-User.welcomeMessage() //now,this welcome message for laura
+// User.username = 'laura' //here, I change the username context/value
+// User.welcomeMessage() //now,this welcome message for laura
 
-console.log(this); 
+// console.log(this); 
 /*
     here the out is empty object cause, in global there is no object..
     
     it return the empty object cause it execute in node platform,
     if we execute in our browser console then it gives some output...
 */
+
+
+
+// function cigratte (){
+//     const userName = 'Teuton Roy'
+//     console.log(this.userName);   it gives undifine, so [this.] keyword can't use in function..
+// }
+// cigratte()
