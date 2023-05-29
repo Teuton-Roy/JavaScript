6d0d53fe877af3abeb90625b849e2077e58d57fb
Teuton Roy <popeyehk404@gmail.com>
2023-05-29 16:23:57 +0530

    Syntax of IIFE

--- Basics-part3/IIFE.js
+++ Basics-part3/IIFE.js
@@ -1,1 +1,19 @@
 //Immediately Invoked Function Expressions (IIFE)
+
+//IIFE function..
+
+(function sutta(){
+    console.log('Sutta-wala, DB-Connected😁');
+})()
+
+
+/*
+    Syntax of IIFE: 
+    
+    1st paranthesis where we define our function ()
+    2nd paranthesis where the function excution heppens ()
+    ()()
+
+
+    
+*/
