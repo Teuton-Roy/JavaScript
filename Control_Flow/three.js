287e74b208c6522070c736c0504ee6a431efb327
Teuton Roy <popeyehk404@gmail.com>
2023-05-31 20:22:06 +0530

    ...

---
+++ Control_Flow/three.js
@@ -0,0 +1,14 @@
+const userEmail = `teu@ai.in`
+
+if (userEmail) {
+    console.log(`This [${userEmail}] email present.`);
+}
+else{
+    console.log(`No use email present.`);
+}
+
+//falsy value:-
+//false, 0, -0, BigInt 0n, "", null, undefine, NaN
+
+//truthy
+//"0",'false', " ", [], {}, function(){}
