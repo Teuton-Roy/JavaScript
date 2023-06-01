692aad5b9d2710c6950706da685bc3a590c67063
Teuton Roy <popeyehk404@gmail.com>
2023-06-01 19:12:39 +0530

    add commit

--- Control_Flow/three.js
+++ Control_Flow/three.js
@@ -1,4 +1,4 @@
-const userEmail = `teu@ai.in`
+const userEmail = []
 
 if (userEmail) {
     console.log(`This [${userEmail}] email present.`);
@@ -12,3 +12,13 @@ else{
 
 //truthy
 //"0",'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log(`Array is empty`);
+// }
+
+//when we get empty object
+const emptyObj = {}
+if (Object.keys(emptyObj).length === 0) {
+    console.log(`Object is empty`);
+}
