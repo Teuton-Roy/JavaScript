776fbda1a496008c7d82a661defd21ee4c8a2831
Teuton Roy <popeyehk404@gmail.com>
2023-06-01 19:26:00 +0530

    Nullish Coalescing Operator

--- Control_Flow/three.js
+++ Control_Flow/three.js
@@ -22,3 +22,13 @@ const emptyObj = {}
 if (Object.keys(emptyObj).length === 0) {
     console.log(`Object is empty`);
 }
+
+
+//Nullish Coalescing Operator (??) :- null and undefined
+
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+
+// val1 = undefined ?? 15
+// console.log(val1);
