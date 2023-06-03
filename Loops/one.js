f144a2b78159efe646dbb1020b38d1a8a6702ff3
Teuton Roy <popeyehk404@gmail.com>
2023-06-03 20:05:12 +0530

    ..

--- Loops/one.js
+++ Loops/one.js
@@ -5,10 +5,18 @@
 //     console.log(element);
 // }
 
-for (let i = 1; i <= 10; i++) {
-    // console.log(`Outer loop value: ${i}`);
-    for (let j = 1; j <= 10; j++) {
-        // console.log(`Inner loop value: ${j}`);
-        console.log(i + '*' + j + '=' + i*j);
-    }
-}
+// for (let i = 1; i <= 10; i++) {
+//     // console.log(`Outer loop value: ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop value: ${j}`);
+//         console.log(i + '*' + j + '=' + i*j);
+//     }
+// }
+
+
+// const arr = ['Teuton', 'Iron-Man', 'Spider-Man', 'Super-Man']
+
+// for(let i = 0; i < arr.length; i++){
+//     const ele = arr[i]
+//     console.log(ele);
+// }
