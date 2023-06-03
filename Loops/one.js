812aa85b82118718b54537e6d4f6bc37a7cbeac6
Teuton Roy <popeyehk404@gmail.com>
2023-06-03 19:46:12 +0530

    ..

--- Loops/one.js
+++ Loops/one.js
@@ -1,6 +1,13 @@
 //for loop
 
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
 for (let i = 0; i <= 10; i++) {
-    const element = i;
-    console.log(element);
-} 
+    console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        console.log(`Inner loop value: ${j}`);;
+    }
+}
