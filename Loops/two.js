fa2d4d08903258a0621242bedf72de9dda6d8764
Teuton Roy <popeyehk404@gmail.com>
2023-06-04 19:10:24 +0530

    ...

---
+++ Loops/two.js
@@ -0,0 +1,15 @@
+//While loop
+
+
+// let i = 0;
+// while ( i <= 10) {
+//     console.log(`value of i is ${i}`)
+//     i += 2;
+// }
+
+// const arr = ['Jagadish ch Roy', 'Tapashi Roy', 'Teuton Roy', 'Tripti Roy']
+// let myArr = 0
+// while(myArr < arr.length){
+//     console.log(`My Family Members are ${arr[myArr]}.`)
+//     myArr++
+// }
