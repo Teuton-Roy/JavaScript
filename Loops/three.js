36f23e8654a153fd788aeb4f990813885d3ed35d
Teuton Roy <popeyehk404@gmail.com>
2023-06-04 19:28:10 +0530

    for of

--- Loops/three.js
+++ Loops/three.js
@@ -8,8 +8,13 @@
 // autometically access every thing when loop need to start, when need to stop, object size every thing
 // }
 
-const arr = [1,2,3,4,5]
+// const arr = [1,2,3,4,5]
 
-for (const num of arr) {
-    console.log(num);
-}
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings = 'Hello World!'
+// for (const greet of greetings) {
+//     console.log(greet);
+// }
