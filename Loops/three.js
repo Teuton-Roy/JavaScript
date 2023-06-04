970167be0143e711227c96cce97a9f0ad1acbc37
Teuton Roy <popeyehk404@gmail.com>
2023-06-04 19:23:27 +0530

    for-of

---
+++ Loops/three.js
@@ -0,0 +1,15 @@
+// (for-of) loop
+
+//syntax..
+// for (const iterator of object) { 
+// iterator:- use normal variable,
+// object:- here, object mean's - kar opr loop use krte chai..
+// here no need to increment or decrement.
+// autometically access every thing when loop need to start, when need to stop, object size every thing
+// }
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
+}
