0322607764230ff2ecb13d6d4041309e67f9e0d0
Teuton Roy <popeyehk404@gmail.com>
2023-06-05 20:08:53 +0530

    using filter to return values from array

--- Loops/six.js
+++ Loops/six.js
@@ -2,8 +2,20 @@
 const coding = ['C/C++','Java','Python','JavaScript','Go']
 
 //for-each loop do not return value.
-const values = coding.forEach((item) =>{
-    console.log(item);
-    return item;
+// const values = coding.forEach((item) =>{
+//     console.log(item);
+//     return item;
+// })
+// console.log(values);
+
+
+
+//for return value.. we using filter.
+
+const myNums= [1,2,3,4,5,6,7,8,9,10]
+const newNums = myNums.filter( (num)=>{
+    if(num > 5){
+        return num;
+    }
 })
-console.log(values);
+console.log(newNums);
