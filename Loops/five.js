6eea47e52c20eb171fd4e8caab65310ccfb99f4f
Teuton Roy <popeyehk404@gmail.com>
2023-06-05 19:39:03 +0530

    using for-each loop in Array-of-Objects.

--- Loops/five.js
+++ Loops/five.js
@@ -19,3 +19,25 @@ const coding = ['C/C++','Java','Python','JavaScript','Go']
 // coding.forEach( (item, index, arr) => {
 //     console.log(item, index, arr);
 // })
+
+
+// Array of Objext
+const myCoding = [
+    {
+        languageName:'JavaScript',
+        languageFileName:'js'
+    },
+    {
+        languageName:'Java',
+        languageFileName:'java'
+    },
+    {
+        languageName:'C/C++',
+        languageFileName:'cpp'
+    }
+]
+
+myCoding.forEach((item) =>{
+    console.log(item.languageName);
+    console.log(item.languageFileName);
+})
