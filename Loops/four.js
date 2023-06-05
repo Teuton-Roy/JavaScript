3900090ea0e6f0125eafbeecb3a9b9fdbab491b5
Teuton Roy <popeyehk404@gmail.com>
2023-06-05 19:19:13 +0530

    for-in loop in Map()

--- Loops/four.js
+++ Loops/four.js
@@ -17,8 +17,22 @@
 
 //for-in loop in Array..
 
-const programming = ['C/C++', 'Java', 'JavaScript', 'Python','Go']
-for (const key in programming) {
-    // console.log(key); //just print Array key and Array-key mean's actually the Array-indexing
-    console.log(`For index ${key} the value is: ${programming[key]}`);
-}
+// const programming = ['C/C++', 'Java', 'JavaScript', 'Python','Go']
+// for (const key in programming) {
+//     // console.log(key); //just print Array key and Array-key mean's actually the Array-indexing
+//     console.log(`For index ${key} the value is: ${programming[key]}`);
+// }
+
+//for-in loop in Maps
+
+//Map() is not iterable so we can't so we can't iterate in loop like this.
+// const map = new Map()
+// map.set('IN', 'India')
+// map.set('USA', 'United State of America')
+// map.set('AUS', 'Australia')
+// map.set('Fr', 'France')
+// map.set('RUS', 'Russia')
+
+// for (const key in map) {
+//     console.log(key);
+// }
