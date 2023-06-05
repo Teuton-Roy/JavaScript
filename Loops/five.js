03ce3006182bbd18f30133bb29c0ad0c2e99d1cc
Teuton Roy <popeyehk404@gmail.com>
2023-06-05 19:30:47 +0530

    using Arrow function.

--- Loops/five.js
+++ Loops/five.js
@@ -2,6 +2,10 @@
 
 const coding = ['C/C++','Java','Python','JavaScript','Go']
 
-coding.forEach( function (i) {
+// coding.forEach( function (i) {
+//     console.log(i);
+// })
+
+coding.forEach( (i) => {
     console.log(i);
-})
+} )
