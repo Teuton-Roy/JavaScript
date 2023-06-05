5e6a350aaa5b6a402bb7f54b5d7a15b49f19a137
Teuton Roy <popeyehk404@gmail.com>
2023-06-05 19:14:52 +0530

    for-in loop in array

--- Loops/four.js
+++ Loops/four.js
@@ -5,12 +5,20 @@
 // }
 
 
+// const myObject = {
+//     js:"javascript",
+//     cpp:"c/c++",
+//     rb:"ruby"
+// }
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
 
-const myObject = {
-    js:"javascript",
-    cpp:"c/c++",
-    rb:"ruby"
-}
-for (const key in myObject) {
-    console.log(`${key} shortcut is for ${myObject[key]}`);
+
+//for-in loop in Array..
+
+const programming = ['C/C++', 'Java', 'JavaScript', 'Python','Go']
+for (const key in programming) {
+    // console.log(key); //just print Array key and Array-key mean's actually the Array-indexing
+    console.log(`For index ${key} the value is: ${programming[key]}`);
 }
