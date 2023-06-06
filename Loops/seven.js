d367804becea561d4fa0079d79024ee9b28f4b91
Teuton Roy <popeyehk404@gmail.com>
2023-06-06 19:42:32 +0530

    chaining

--- Loops/seven.js
+++ Loops/seven.js
@@ -15,8 +15,10 @@
 const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 
 const newNums = myNumbers.map ((num)=> {
-    return num+10
+    return num*10
 }).map((num)=>{
     return num+1
+}).filter((num)=>{
+    return num > 40
 })
 console.log(newNums);
