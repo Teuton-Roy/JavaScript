cd83070dc0f1e264432fc7330c7d63d5cb8718d1
Teuton Roy <popeyehk404@gmail.com>
2023-06-06 19:41:02 +0530

    chaining

--- Loops/seven.js
+++ Loops/seven.js
@@ -1,9 +1,22 @@
 //Map
 
+// const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNumbers.map ((num)=>{
+//     return num+10
+// })
+
+// console.log(newNums);
+
+//Chaning:- mean's more than one method use simulteneously
+/*
+    const newNums = myNumbers.map().map().filter()
+*/
 const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 
-const newNums = myNumbers.map ((num)=>{
+const newNums = myNumbers.map ((num)=> {
     return num+10
+}).map((num)=>{
+    return num+1
 })
-
 console.log(newNums);
