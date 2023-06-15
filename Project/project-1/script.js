bb6f303e605df0bb6fc24ae30b174d12d396f3b1
Teuton Roy <popeyehk404@gmail.com>
2023-06-15 19:38:29 +0530

    Select Body and select buttons

---
+++ Project/project-1/script.js
@@ -0,0 +1,8 @@
+//Now adding functionality for color switcher so that it change the background color
+
+//select buttons
+const buttons = document.querySelectorAll('.button')
+// console.log(buttons);
+
+//Select Body
+const body = document.querySelector('body')
