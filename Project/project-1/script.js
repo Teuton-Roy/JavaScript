fc696d512cae148cd76fe74645fbf7895cb19505
Teuton Roy <popeyehk404@gmail.com>
2023-06-15 19:59:51 +0530

    add commit

--- Project/project-1/script.js
+++ Project/project-1/script.js
@@ -19,5 +19,14 @@ buttons.forEach((button) =>{
         if (e.target.id === 'grey') {
             body.style.backgroundColor = e.target.id;
         }
+        if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id;
+        }
     })
 })
