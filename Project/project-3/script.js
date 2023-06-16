f34326e24a918906652e43353e3acaa0e4cae6a9
Teuton Roy <popeyehk404@gmail.com>
2023-06-16 20:24:54 +0530

    set time

--- Project/project-3/script.js
+++ Project/project-3/script.js
@@ -1,5 +1,10 @@
 //select clock
-const colck = document.getElementById('clock')
+const clock = document.getElementById('clock')
 
-let date = new Date();
-console.log(date.toLocaleTimeString());
+// let date = new Date();
+// console.log(date.toLocaleTimeString());
+
+setInterval(()=>{
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString()
+},1000)
