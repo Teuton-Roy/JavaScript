f19a69bbf9bd19ffccacbd9ea1ecc6f6393e4c00
Teuton Roy <popeyehk404@gmail.com>
2023-06-16 20:03:17 +0530

    updated script

--- Project/project-2/script.js
+++ Project/project-2/script.js
@@ -7,4 +7,18 @@ form.addEventListener('submit', (e)=>{
 
     const height = parseInt(document.querySelector('#height').value) //get height value in string form and Using parseInt it convert into integer//
     const weight = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+
+    if (height ==='' || height < 0 || isNaN(height)) {
+        results.innerHTML = `Please give a valid height ${height}`
+    }
+    else if (weight ==='' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `Please give a valid weight ${weight}`
+    }
+    
+    else{
+        const BMI = (weight / ((height*height)/10000)).toFixed(2)
+        //show the result
+        results.innerHTML = `<span>${BMI}</span>`
+    }
 })
