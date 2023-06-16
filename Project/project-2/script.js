35916e81f04729ad3023d9b19ed5ab3d2ad30fd8
Teuton Roy <popeyehk404@gmail.com>
2023-06-16 19:50:58 +0530

    add commit on event Listener

--- Project/project-2/script.js
+++ Project/project-2/script.js
@@ -1,3 +1,10 @@
 //select form
 const form = document.querySelector('form')
 
+//add event Listener in form
+form.addEventListener('submit', (e)=>{
+    e.preventDefault() //stop default actions
+
+    const height = parseInt(document.querySelector('#height').value) //get height value in string form and Using parseInt it convert into integer//
+    const weight = parseInt(document.querySelector('#weight').value)
+})
