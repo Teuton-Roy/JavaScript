9276e1c616252e30f9709b640c16d9680f859de7
Teuton Roy <popeyehk404@gmail.com>
2023-06-17 19:25:39 +0530

    select all the classes and id's

--- Project/project-4/script.js
+++ Project/project-4/script.js
@@ -1,2 +1,11 @@
 //Generate Random number
 const randomNumber = console.log(parseInt(Math.random()*100+1))
+
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+const lowOrHi = document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.resultParas')
+
+const p =document.createElement('p')
